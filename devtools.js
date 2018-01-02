7b5cc72449e53bc72a22a10933b627107ce16c50
Mark Chambers <marksy@Rio.home>
2018-01-02 14:36:23 +0000

    encapsulated file and rm min.js

--- devtools.js
+++ devtools.js
@@ -1,21 +1,23 @@
-let viewportX = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-let viewportY = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-let docY;
-let div = document.createElement('div');
+(function() {
+  'use strict';
+  let viewportX = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  let viewportY = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  let div = document.createElement('div');
 
-div.setAttribute('id', 'dev-tools');
-document.body.appendChild(div);
-div.classList.add('show');
-div.onmouseover = function(){
-  div.classList.toggle('switch');
-};
+  div.setAttribute('id', 'dev-tools');
+  document.body.appendChild(div);
+  div.classList.add('show');
+  div.onmouseover = function(){
+    div.classList.toggle('switch');
+  };
 
-window.addEventListener('resize', init);
+  window.addEventListener('resize', init);
 
-function init() {
-    viewportX = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    viewportY = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-    div.innerHTML = '<span class="alt-color">width:</span>' + viewportX + 'px <span class="alt-color">height:</span>' + viewportY + 'px';
-};
+  function init() {
+      viewportX = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      viewportY = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+      div.innerHTML = '<span class="alt-color">width:</span>' + viewportX + 'px <span class="alt-color">height:</span>' + viewportY + 'px';
+  };
 
-init();
+  init();
+}());
